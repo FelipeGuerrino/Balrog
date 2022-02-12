4f19d6e58e74735e962f4f54a371fd51c5e2edec
FelipeGuerrino <felipe_bg2005@hotmail.com>
2022-02-12 11:41:50 -0300

    deprecation

--- src/server.py
+++ src/server.py
@@ -1,17 +1,18 @@
-from flask import Flask
-from threading import Thread
+# from flask import Flask
+# from threading import Thread
 
-app = Flask('')
+# app = Flask('')
 
-@app.route('/')
-def home():
-    return "Bot is running..."
+# @app.route('/')
+# def home():
+#     return "Bot is running..."
 
-def run():
-    app.run(host="0.0.0.0", port=8080)
+# def run():
+#     app.run(host="0.0.0.0", port=8080)
 
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
+# def keep_alive():
+#     t = Thread(target=run)
+#     t.start()
 
 #Server script (hosted on replit)
+"""Deprecated!!!"""
